7590c91685a31bb0161fd6bba7307fed03c65015
Aleksander-Kovalenko <coldfellow@gmail.com>
2021-10-24 09:01:03 +0300

    done

--- js/countdownTimer.js
+++ js/countdownTimer.js
@@ -7,14 +7,17 @@ export default class Timer {
 
     this.start = this.startTimer();
   }
+  timerID = null;
 
   updateCountDown() {
-    setTimeout(this.startTimer.bind(this), 1000);
+    this.timerID = setTimeout(this.startTimer.bind(this), 1000);
   }
 
   startTimer() {
     let now = new Date();
     let date = this.refs.date;
+    if (date - now <= 0) return this.convertDate(0);
+
     const time = this.diffSubtract(now, date);
     let res = new Date(time);
 
@@ -27,32 +30,12 @@ export default class Timer {
   }
 
   convertDate(time) {
-    // console.log(currentDate - time);
-    /*
-     * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-     * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-     */
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-
-    /*
-     * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-     * остатка % и делим его на количество миллисекунд в одном часе
-     * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-     */
     const hours = this.pad(
       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     );
-    /*
-     * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-     * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-     */
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    /*
-     * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-     * миллисекунд в одной секунде (1000)
-     */
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-
     this.render(days, hours, mins, secs);
   }
 
@@ -68,8 +51,3 @@ export default class Timer {
     return String(value).padStart(2, "0");
   }
 }
-
-// const timer = new Timer({
-//   selector: "timer-1",
-//   date: new Date("Jul 17, 2022"),
-// });
--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import Timer from "./countdownTimer.js";
 
 const timer = new Timer({
   selector: "timer-1",
-  date: new Date("Jul 17, 2022"),
+  date: new Date("Oct 26, 2021"),
 });
 
 timer.startTimer();
